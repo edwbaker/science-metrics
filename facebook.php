61c9b3d9cace87e2b9204e49ab307bafeac7b677
edwbaker <edwbaker@gmail.com>
2013-10-03 12:58:34 +0100

    Count Facebook shares.

---
+++ facebook.php
@@ -0,0 +1,23 @@
+<?php
+$facebook_endpoint = "http://graph.facebook.com/";
+
+//SAMPLE USE
+$urls = array("http://biodiversitydatajournal.com/articles.php?id=995", "http://dx.doi.org/10.3897/BDJ.1.e995");
+print facebook_get_share_count($urls);
+//-----------------------------------
+
+
+
+//Function allows for an array to be passed, for pages with multiple URLs
+function facebook_get_share_count($urls) {
+	if (!is_array($urls)) {
+		$urls = array($urls);
+	}
+	$count = 0;
+	global $facebook_endpoint;
+	foreach ($urls as $url) {
+	  $facebook_response = json_decode(file_get_contents($facebook_endpoint.urlencode($url)));
+	  $count += $facebook_response->shares;
+	}
+	return $count;
+}
